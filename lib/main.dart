854b5d6aed411c54ebb2b36980831dd66aabea6d
Yassine <80011492+Yassine-ch@users.noreply.github.com>
2022-12-01 17:19:04 +0100

    buyIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:ecommerce/firebase_options.dart';
 import 'package:ecommerce/screens/login_screen.dart';
 import 'package:ecommerce/screens/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-main()=> runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
